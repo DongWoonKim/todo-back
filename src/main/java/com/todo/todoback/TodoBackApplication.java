0cb9be7007d411eed5743bb887f62f1820b59c87
ghysnzsy <30285849+ghysnzsy@users.noreply.github.com>
2022-06-11 16:21:46 +0900

    Pull Request용 Branc h
    이전 Pull Request Close 요청

--- src/main/java/com/todo/todoback/TodoBackApplication.java
+++ src/main/java/com/todo/todoback/TodoBackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoBackApplication {
-
+    //Pull Request용 Jongyeon. 이전 Pull Request Close 요청
     public static void main(String[] args) {
         SpringApplication.run(TodoBackApplication.class, args);
     }
